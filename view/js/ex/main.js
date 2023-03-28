691dd30fee23307c5f74c654a45a7914a51b37bb
FelixAhn <fahn0719@gmail.com>
2023-03-28 20:35:18 +0900

    design change

--- view/js/ex/main.js
+++ view/js/ex/main.js
@@ -1,65 +1,194 @@
-if(document.location.pathname === '/ex') {
+if (document.location.pathname === '/ex') {
     document.getElementById('main_data').innerHTML = `
-        <header class="bg-dark py-5">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center justify-content-center">
-                    <div class="col-lg-8 col-xl-7 col-xxl-6">
-                        <div class="my-5 text-center text-xl-start">
-                            <h1 class="display-5 fw-bolder text-white mb-2">인클루더 동아리</h1>
-                            <p class="lead fw-normal text-white-50 mb-4">인클루더는 2022년에 설립되어서 2023년에 정식 승격된 한국해양대학교 정식 프로그래밍 동아리입니다.</p>
-                            <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                <a class="btn btn-primary btn-lg px-4 me-sm-3" href="/ex/intro">자세히 알아보기</a>
-                            </div>
-                        </div>
+    <header class="py-5 mb-5 top-h" id="top">
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center justify-content-center">
+            <div class="col-lg-8 col-xl-7 col-xxl-6">
+                <div class="my-5 text-center text-xl-start">
+                    <h1 class="display-5 fw-bolder text-white mb-2">인클루더 동아리</h1>
+                    <div class="divider"></div>
+                    <p class="lead fw-normal text-white-50 mb-4">인클루더는 2022년에 설립되어서 2023년에 정식 승격된 한국해양대학교 정식
+                        프로그래밍 동아리입니다.</p>
+                    <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
+                        <a class="btn btn-success btn-lg px-4 me-sm-3" href="#active">자세히 알아보기</a>
                     </div>
-                    <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center"><img class="img-fluid rounded-3 my-5" src="/view/img/lang_file.jpg" alt="..."></div>
                 </div>
             </div>
-        </header>
-        <br>
-        <br>
-        <div class="container" id="featured-3">
-            <div class="container px-4" id="featured-3">
-                <h2 class="pb-2 border-bottom">진행 중인 분반</h2>
-                <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-                    <div class="feature col">
-                        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" style="margin: 10px;" fill="currentColor" class="bi bi-globe2" viewBox="0 0 16 16">
-                                <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855-.143.268-.276.56-.395.872.705.157 1.472.257 2.282.287V1.077zM4.249 3.539c.142-.384.304-.744.481-1.078a6.7 6.7 0 0 1 .597-.933A7.01 7.01 0 0 0 3.051 3.05c.362.184.763.349 1.198.49zM3.509 7.5c.036-1.07.188-2.087.436-3.008a9.124 9.124 0 0 1-1.565-.667A6.964 6.964 0 0 0 1.018 7.5h2.49zm1.4-2.741a12.344 12.344 0 0 0-.4 2.741H7.5V5.091c-.91-.03-1.783-.145-2.591-.332zM8.5 5.09V7.5h2.99a12.342 12.342 0 0 0-.399-2.741c-.808.187-1.681.301-2.591.332zM4.51 8.5c.035.987.176 1.914.399 2.741A13.612 13.612 0 0 1 7.5 10.91V8.5H4.51zm3.99 0v2.409c.91.03 1.783.145 2.591.332.223-.827.364-1.754.4-2.741H8.5zm-3.282 3.696c.12.312.252.604.395.872.552 1.035 1.218 1.65 1.887 1.855V11.91c-.81.03-1.577.13-2.282.287zm.11 2.276a6.696 6.696 0 0 1-.598-.933 8.853 8.853 0 0 1-.481-1.079 8.38 8.38 0 0 0-1.198.49 7.01 7.01 0 0 0 2.276 1.522zm-1.383-2.964A13.36 13.36 0 0 1 3.508 8.5h-2.49a6.963 6.963 0 0 0 1.362 3.675c.47-.258.995-.482 1.565-.667zm6.728 2.964a7.009 7.009 0 0 0 2.275-1.521 8.376 8.376 0 0 0-1.197-.49 8.853 8.853 0 0 1-.481 1.078 6.688 6.688 0 0 1-.597.933zM8.5 11.909v3.014c.67-.204 1.335-.82 1.887-1.855.143-.268.276-.56.395-.872A12.63 12.63 0 0 0 8.5 11.91zm3.555-.401c.57.185 1.095.409 1.565.667A6.963 6.963 0 0 0 14.982 8.5h-2.49a13.36 13.36 0 0 1-.437 3.008zM14.982 7.5a6.963 6.963 0 0 0-1.362-3.675c-.47.258-.995.482-1.565.667.248.92.4 1.938.437 3.008h2.49zM11.27 2.461c.177.334.339.694.482 1.078a8.368 8.368 0 0 0 1.196-.49 7.01 7.01 0 0 0-2.275-1.52c.218.283.418.597.597.932zm-.488 1.343a7.765 7.765 0 0 0-.395-.872C9.835 1.897 9.17 1.282 8.5 1.077V4.09c.81-.03 1.577-.13 2.282-.287z"/>
-                            </svg>
-                        </div>
-                        <h3 class="fs-2">프로젝트팀</h3>
-                        <p>다양한 웹과 앱 프로젝트를 하면서 현업에 대한 지식을 늘리는 팀입니다.</p>
-                        <a href="/ex/project" class="icon-link d-inline-flex align-items-center">
-                            자세히 알아보기
-                        </a>
+            <div class="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
+                <img class="img-fluid my-5 in_logo" src="/view/img/logo.jpg" alt="..."
+                    style="height: 500px; width: 500px;">
+            </div>
+        </div>
+    </div>
+</header>
+
+<!-- intro -->
+<section id="intro">
+    <div class=" d-flex justify-content-center align-items-center pb-2 mb-5 overlay">
+        <div class="container-xxl">
+            <div class="row align-items-top">
+                <!-- 왼쪽 부분 -->
+                <div class="col-md-3 welcome position-relative">
+                    <h1 class="welcome-title fw-light"><span class="text-success fw-bold">INCLUDER</span></h1>
+                    <h3 class="welcome-subtitle fw-light"><span class="text-success fw-bold">를 소개합니다</span></h3>
+                    <img src="/view/img/hello.jpg" alt="" class="position-absolute intro__logo"
+                        style="height: 250px; width: 250px; left: -30; top: 100;">
+                </div>
+                <!-- 오른쪽 부분 -->
+                <div class="col-md-9 d-md-block intro">
+                    <div class="intro my-3">
+                        <h4 class="intro-title card-title mb-3 fw-bold">동아리 소개글</h4>
+                        <p class="card-text fs-5">😀안녕하세요 저희는 이번에 정식 동아리로 승격된 한국해양대 프로그래밍 동아리
+                            "INCLUDER"
+                            입니다.😀 <br><br>
+
+                            저희 인클루더를 소개 드리자면 <br><br>
+
+                            ⚡ C언어, Python, Web, ML 등 동아리 스터디 그룹 운영 <br>
+                            ⚡ 스터디 자체 모임은 주 1회 , 동아리 정기 모임은 2주에 한번씩 운영하고 있습니다 <br>
+                            ⚡ 예섬관에 동아리방이 있습니다! <br>
+                            ⚡ 체계를 잡아가는 단계이니 만큼 여러분의 의견을 적극 수렴하여 반영 가능합니다! <br><br>
+
+                            저희 인클루더 에서 다음과 같은 분들을 찾습니다 👍<br><br>
+
+                            🚩 소속 학부,학년 무관하게 코딩,컴퓨터 관련 주제에 관심 있으신 분 (몰라도 됨) <br>
+                            🚩 코딩을 접해보고 싶으신 분<br>
+                            🚩 그냥 동아리 활동이 하고 싶으신 분<br> </p>
                     </div>
-                    <div class="feature col">
-                        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" style="margin: 10px;" fill="currentColor" class="bi bi-filetype-py" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M14 4.5V14a2 2 0 0 1-2 2H7v-1h5a1 1 0 0 0 1-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM0 11.85h1.6c.289 0 .533.06.732.179.201.117.355.276.46.477.105.201.158.427.158.677 0 .25-.054.476-.16.677-.106.199-.26.357-.464.474a1.452 1.452 0 0 1-.732.173H.79v1.342H0V11.85Zm2.06 1.714a.795.795 0 0 0 .085-.381c0-.227-.062-.4-.185-.521-.123-.122-.294-.182-.513-.182H.788v1.406h.66a.794.794 0 0 0 .374-.082.574.574 0 0 0 .238-.24Zm2.963.75v1.535H4.23v-1.52L2.89 11.85h.876l.853 1.696h.032l.856-1.696h.855l-1.339 2.464Z"/>
-                            </svg>
-                        </div>
-                        <h3 class="fs-2">파이썬팀</h3>
-                        <p>파이썬을 배우면서 프로그래밍의 기초를 배우는 팀입니다.</p>
-                        <a href="/ex/study" class="icon-link d-inline-flex align-items-center">
-                        자세히 알아보기
-                        </a>
-                    </div>
-                    <div class="feature col">
-                        <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48px" height="48px" style="margin: 10px;" fill="currentColor" class="bi bi-braces-asterisk" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C2.25 2 1.49 2.759 1.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6ZM14.886 7.9v.164c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456v-1.3c-1.114 0-1.49-.362-1.49-1.456V4.352C14.51 2.759 13.75 2 12.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6ZM7.5 11.5V9.207l-1.621 1.621-.707-.707L6.792 8.5H4.5v-1h2.293L5.172 5.879l.707-.707L7.5 6.792V4.5h1v2.293l1.621-1.621.707.707L9.208 7.5H11.5v1H9.207l1.621 1.621-.707.707L8.5 9.208V11.5h-1Z"/>
-                            </svg>
-                        </div>
-                        <h3 class="fs-2">C언어팀</h3>
-                        <p>C언어를 배워봅시다.</p>
-                        <a href="/ex/study" class="icon-link d-inline-flex align-items-center">
-                            자세히 알아보기
-                        </a>
-                    </div>
+
                 </div>
             </div>
         </div>
+    </div>
+</section>
+
+<!-- Active -->
+<section id="active">
+    <div class="container p-5 mb-4 rounded-5 bg-light">
+        <div class="active_title text-center">
+            <img src="/view/img/study.jpg" alt="" style="height: 250px;" class="active__logo">
+            <h2 class="fw-bold mb-5 mt-3">우리 동아리는 이런 활동을 해요!</h2>
+        </div>
+        <div class="d-flex flex-column">
+            <div class="row">
+                <p class="bg-white p-2 rounded-4 text-center align-items-center shadow-lg lh-lg"><b
+                        class="h3 fw-bold">C언어</b><br>
+                    1. c언어반 스터디는 c언어를 공부하고자 하는 분들을 위한 스터디 모임입니다. <br>
+                    2. c언어는 컴퓨터 공학 분야에서 가장 기본이 되는 언어 중 하나로, 프로그래밍을 처음
+                    시작하는 분들에게 매우 적합한 언어입니다. <br>
+                    스터디 모임에서는 c언어의 기본 문법부터 시작하여, 변수, 함수, 포인터, 배열, 구조체 등의 개념을 다룹니다. <br>
+                    3. 스터디 모임은 매주 한 번씩 정기적으로 진행되며, 참여자들끼리 서로 질문하고 답변하는 시간도 가지고 있습니다. <br>
+                    4. c언어반 스터디는 c언어를 처음 배우는 분들뿐만 아니라, 이미 약간의 경험이 있으신 분들도 참여하실 수 있습니다. 함께 c언어를 공부하고 싶으신 분들은
+                    언제든지 참여해 주시기 바랍니다. <br>
+                </p>
+            </div>
+            <div class="row">
+                <p class="bg-white p-2 rounded-4 text-center align-items-center shadow-lg lh-lg"><b
+                        class="h3 fw-bold">파이썬</b> <br>
+                    1. 파이썬반 스터디는 파이썬 언어를 공부하고자 하는 분들을 위한 스터디 모임입니다. <br>
+                    2. 파이썬은 현재 가장 인기있는 프로그래밍 언어 중 하나로, <br> 데이터 분석, 인공지능, 웹프로그래밍 등 다양한 분야에서 활용되고 있습니다. <br>
+                    3. 모임에서는 파이썬의 기본 문법부터 시작하여, 변수, 함수, 클래스, 모듈 등의 개념을 다룹니다.<br>
+                    4. 스터디 모임은 매주 한 번씩 정기적으로 진행되며, 참여자들끼리 서로 질문하고 답변하는 시간도 가지고 있습니다. 또한, 모임 후에는 스터디 내용을 정리하여
+                    공유하는 시간도 가지고 있습니다. <br>
+                    5. 파이썬반 스터디는 파이썬을 처음 배우는 분들뿐만 아니라, 이미 약간의 경험이 있으신 분들도 참여하실 수 있습니다. 함께 파이썬을 공부하고 싶으신 분들은
+                    언제든지 참여해 주시기 바랍니다.</p>
+            </div>
+
+            <div class="row">
+                <p class="bg-white p-2 rounded-4 text-center align-items-center shadow-lg lh-lg"><b
+                        class="h3 fw-bold">프로젝트</b> <br>
+                    1. 웹 프로젝트 스터디는 웹 개발에 관심이 있는 분들이 모여 함께 프로젝트를 기획하고 개발하는 스터디 모임입니다. <br>
+                    2. HTML, CSS, JavaScript 등의 웹 프론트엔드 기술부터, <br>
+                    Django, Node.js 등의 백엔드 기술까지 다양한 기술을 활용하여 웹 프로젝트를 구현해볼 수 있습니다. <br>
+                    3. 스터디 모임에서는 프로젝트 기획부터 시작하여, 디자인, 개발, 배포까지의 전과정을 함께 공부합니다. <br>
+                    프로젝트의 기획과 디자인은 스터디 모임 내에서 직접 수행하며, 개발과 배포는 모임 내에서 개인 또는 팀 단위로 수행합니다. <br>
+                    4. 스터디 모임은 매주 한 번씩 정기적으로 진행되며, 각자 맡은 역할에 대해 서로 의견을 나누고 협업하는 시간도 가지고 있습니다. <br>
+                    5. 웹 프로젝트 스터디는 웹 개발에 대한 지식이 없더라도 참여하실 수 있습니다. <br>
+                    함께 웹 개발을 공부하고 싶으신 분들은 언제든지 참여해 주시기 바랍니다.
+                </p>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- ban -->
+<section id="ban">
+    <div class="container" id="featured-3">
+        <div class="container px-4 mb-4" id="featured-3">
+            <h2 class="pb-2 mb-2 border-bottom fw-bold">진행 중인 분반</h2>
+            <div class="row">
+                <div class="col-lg-4 position-relative p-2 ban rounded-5 border-start border-2"
+                    style="height: 400px;">
+                    <img src="/view/img/lang_file.jpg" alt="" class="img-fluid rounded-5 my-2 shadow mb-4"
+                        style="height: 180px; width: 180px;">
+                    <h2 class="fw-normal">프로젝트반</h2>
+                    <p>다양한 웹과 앱 프로젝트를 하면서 현업에 대한 <br>지식을 늘리는 반입니다.</p>
+                    <p><a class="position-absolute bottom-2 btn btn-success" href="/project">더 알아보기 &raquo;</a>
+                    </p>
+                </div>
+
+                <div class="col-lg-4 position-relative p-2 ban rounded-5 border-start border-2"
+                    style="height: 400px;">
+                    <img src="/view/img/c.jpg" alt="" class="img-fluid rounded-5 my-2 shadow mb-4"
+                        style="height: 180px; width: 180px;">
+                    <h2 class="fw-normal">C언어반</h2>
+                    <p>컴퓨터 공학 분야에서 가장 기본이 되는 언어인 <br>C언어를 배우는 반입니다.</p>
+                    <p><a class="position-absolute bottom-2 btn btn-success" href="/study">더 알아보기 &raquo;</a>
+                    </p>
+                </div>
+
+
+                <div class="col-lg-4 position-relative p-2 ban rounded-5 border-start border-2"
+                    style="height: 400px;">
+                    <img src="/view/img/python.jpg" alt="" class="img-fluid rounded-5 my-2 shadow mb-4"
+                        style="height: 180px; width: 180px;">
+                    <h2 class="fw-normal">파이썬반</h2>
+                    <p>현재 가장 인기있는 프로그래밍 언어 중 하나인 <br> 파이썬 기초를 배우는 반입니다.</p>
+                    <p><a class="position-absolute bottom-2 btn btn-success" href="/study">더 알아보기 &raquo;</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- FAQ -->
+<section id="faq">
+    <div class="container py-5 faq">
+        <!--Section: FAQ-->
+        <h3 class="text-center mb-4 pb-2 text-success fw-bold">FAQ</h3>
+        <div class="row">
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-success"><i class="far fa-paper-plane text-success pe-2"></i> 가입 방법이 무엇인가요?
+                </h6>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, est?
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-success"><i class="fa-solid fa-computer text-success pe-2"></i>컴퓨터에 대한 지식이
+                    하나도
+                    없습니다.. 괜찮을까요?</h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, tempora..
+                </p>
+            </div>
+
+            <div class="col-md-6 col-lg-4 mb-4">
+                <h6 class="mb-3 text-success"><i class="fas fa-user text-success pe-2"></i> 스터디는 어떤 형식으로 진행이
+                    되나요?
+                </h6>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, labore?
+                </p>
+            </div>
+        </div>
+        
+    </div>
+</section>
+</div>
     `;
 }
