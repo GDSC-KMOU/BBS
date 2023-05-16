e3f5cae2a58a69087469c2a6e451f5d2bedec359
잉여개발기 (SPDV) <min08101@naver.com>
2023-05-17 05:38:01 +0900

    add preview

--- view/js/ex/board_add.js
+++ view/js/ex/board_add.js
@@ -15,8 +15,10 @@ if(document.location.pathname.startsWith('/ex/board_add/')) {
                             <textarea id="board_add_content" class="form-control" rows="15" placeholder="내용" aria-label="내용"></textarea>
                             <br>
                             <button type="submit" class="btn btn-success" id="board_add_save">저장</button>
+                            <button type="submit" class="btn btn-outline-success me-2" id="board_add_preview">미리보기</button>
                             <br>
                             <br>
+                            <div id="board_add_preview_field"></div>
                         </div>
                     </div>
                 </div>
@@ -43,6 +45,24 @@ if(document.location.pathname.startsWith('/ex/board_add/')) {
                 document.location.pathname = '/ex/board_read/' + url_encode(board_name) + '/' + text.id;
             } else {
                 alert(text.req + '\n' + text.reason);
+            }
+        });
+    });
+
+    document.getElementById('board_add_preview').addEventListener("click", function() {
+        let content = document.getElementById('board_add_content').value;
+
+        fetch("/api/board_preview", {
+            method : 'POST',
+            headers : { 'Content-Type': 'application/json' },
+            body : JSON.stringify({
+                'content' : content
+            })
+        }).then(function(res) {
+            return res.json();
+        }).then(function(text) {
+            if(text.req === 'ok') {
+                document.getElementById('board_add_preview_field').innerHTML = text.data;
             }
         });
     });
