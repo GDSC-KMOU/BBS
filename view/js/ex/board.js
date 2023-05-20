934d1951d52d792c11a338444a00e86038b015c4
pekov22 <woosoo311@gmail.com>
2023-05-21 00:24:22 +0900

    Updata ex/board.js
    ex/board.js도 수정

--- view/js/ex/board.js
+++ view/js/ex/board.js
@@ -3,16 +3,41 @@
 if(document.location.pathname.startsWith('/ex/board/')) {
     let board_name = document.location.pathname.split('/')[3];
 
-    fetch("/api/board/" + url_encode(board_name)).then(function(res) {
+    fetch("/api/board/" + url_encode(board_name)).then(function (res) {
         return res.json();
-    }).then(function(text) {
+    }).then(function (text) {
         let data = '';
+        let now = new Date();
+        now=new Date().getTime();
+        let the_str='0'
         for (let for_a = 0; for_a < text.length; for_a++) {
+            let past_time=new Date(text[for_a]['date'].replace(/-/g, '/'));
+            past_time=past_time.getTime();
+            
+            let the_time= new Date(now - past_time); 
+            the_time= the_time/1000; //ms->s
+            
+            if (the_time<60){
+                the_str= String(Math.floor(the_time)) + "초전";
+            } else if(the_time<3600){ //1h
+                the_str= String(Math.floor(the_time / 60)) + "분전";
+            } else if(the_time<86400){ //1d
+                the_str= String(Math.floor(the_time / 3600)) + "시간전";
+            } else if(the_time<604800){  //7d
+                the_str= String(Math.floor(the_time / 86400)) + "일전";
+            } else if(the_time<2592000){ //30d
+                the_str= String(Math.floor(the_time / 604800)) + "주일전";
+            } else if(the_time<7776000){ //90d
+                the_str= String(Math.floor(the_time / 2592000)) + "달 전";
+            } else{
+                the_str=Number(the_str);
+                the_str=text[for_a]['date'].split(' ')[0];
+            }
             data += `
                 <tr>
                     <td>` + text[for_a]['doc_id'] + `</td>
-                    <td><a class="text-decoration-none text-success" href="/ex/board_read/` + url_encode(board_name) + '/' + text[for_a]['doc_id'] + `">` + xss_filter(text[for_a]['title']) + `</a></td>
-                    <td>` + text[for_a]['date'].split(' ')[1] + `</td>
+                    <td><a class="text-decoration-none text-success" href="/board_read/` + url_encode(board_name) + '/' + text[for_a]['doc_id'] + `">` + xss_filter(text[for_a]['title']) + `</a></td>
+                    <td>` + the_str + `</td>
                     <td>` + xss_filter(text[for_a]['user_name_real']) + `</td>
                 </tr>
             `;
@@ -21,7 +46,7 @@ if(document.location.pathname.startsWith('/ex/board/')) {
         document.getElementById('main_data').innerHTML = `
             <section id="board">
                 <div class="container-xxl p-5 board-content">
-                    <a href="/ex/board_add/` + url_encode(board_name) + `" class="text-decoration-none text-success">(글쓰기)</a>
+                    <a href="/board_add/` + url_encode(board_name) + `" class="text-decoration-none text-success">(글쓰기)</a>
                     <br>
                     <div class="row gap-5">
                         ` + bbs_nav() + `
