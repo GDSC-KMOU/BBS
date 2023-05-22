016c10a1550cf8b9aba1bea16391d3e4e5293e0a
pekov22 <woosoo311@gmail.com>
2023-05-22 23:05:44 +0900

    Update study.js
    (작성시간의 hour) + (현시각과 작성 시간의 차이) ≥24시간 일때 완료처리

--- view/js/ex/study.js
+++ view/js/ex/study.js
@@ -4,9 +4,33 @@ if(document.location.pathname === '/ex/study') {
     fetch("/api/study").then(function(res) {
         return res.json();
     }).then(function(text) {
+        let now= new Date();
+        let now_unix= now.getTime(); //A
         let data = '';
         for (let for_a = 0; for_a < text.length; for_a++) {
-            data += `
+            let past= new Date(text[for_a]['date'].replace(/-/g, '/'));
+            let past_HourToSec = past.getHours()*3600 
+            let past_unix = past.getTime(); 
+            let the_time=(now_unix-past_unix)/1000; 
+            if ((the_time+past_HourToSec)>=86400) {
+                data += `
+                <div class="d-flex text-muted pt-3">
+                    <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
+                        <title>상태</title>
+                        <rect width="100%" height="100%" fill="#46cb18"></rect>
+                    </svg>
+
+                    <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+                        <div class="d-flex justify-content-between">
+                            <strong class="text-gray-dark">` + xss_filter(text[for_a].team_name) + ` (` + xss_filter(text[for_a].user_name_real) + `)</strong>
+                            ` + xss_filter(text[for_a].date) + `
+                        </div>
+                        <span class="d-block">` + xss_filter(text[for_a].content) + ` <a class="text-decoration-none text-success" href="/ex/study_edit/` + text[for_a].doc_id + `">(수정)</a></span>
+                    </div>
+                </div>
+            `;
+            }else{
+                data += `
                 <div class="d-flex text-muted pt-3">
                     <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false">
                         <title>상태</title>
@@ -21,7 +45,8 @@ if(document.location.pathname === '/ex/study') {
                         <span class="d-block">` + xss_filter(text[for_a].content) + ` <a class="text-decoration-none text-success" href="/ex/study_edit/` + text[for_a].doc_id + `">(수정)</a></span>
                     </div>
                 </div>
-            `;
+            `;  
+            }
         }
 
         document.getElementById('main_data').innerHTML = `
