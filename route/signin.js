0e8f82e25fdb86ebb6b3830a7966ad727b03eb3a
잉여개발기 (SPDV) <min08101@naver.com>
2023-07-30 18:18:17 +0900

    API 코드를 이용한 로그인 추가

--- route/signin.js
+++ route/signin.js
@@ -3,86 +3,108 @@ const sha3_512 = require('js-sha3').sha3_512;
 const func = require('./func.js');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
+function signin_val(db, req, res) {
+    let data = req.body;
+
+    let user_name = data.user_name;
+    let password = data.password;
+
+    db.all("select set_data from user_data where user_name = ? and set_name = 'password' limit 1", [user_name], function(err, db_data) {
+        if(db_data.length === 0) {
+            res.json({
+                "req" : "error",
+                "reason" : "user_name not exist"
+            });
+            db.close();
+        } else {
+            let db_password = db_data[0].set_data;
+            db.all("select set_data from set_data where set_name = 'secret_key'", [], function(err, db_data) {
+                let random_key = '';
+                if(db_data.length !== 0) {
+                    random_key = db_data[0].set_data;
+                }
+
+                if(db_password !== sha3_512(password + random_key)) {
+                    res.json({
+                        "req" : "error",
+                        "reason" : "password !== db_password"
+                    });
+                    db.close();
+                } else {
+                    req.session['user_name'] = user_name;
+                    res.json({ "req" : "ok" });
+                    db.close();
+                }
+            });
+        }
+    });
+}
+
 function signin(req, res) {
     const db = new sqlite3.Database(__dirname + '/../data.db');
 
     let data = req.body;
 
-    let user_name = data.user_name;
-    let password = data.password;
     let hcaptcha_res = data.captcha;
+    let api_code = data.api_code;
 
-    // user_name, set_name, user_data, set_data
-    db.all("select set_data from set_data where set_name = 'hcaptcha_secret'", [], function(err, db_data_2) {
-        let hcaptcha = '';
-        if(db_data_2.length !== 0) {
-            hcaptcha = db_data_2[0].set_data;
-        }
-
-        let post_data = new URLSearchParams();
-        post_data.append('response', hcaptcha_res);
-        post_data.append('secret', hcaptcha);
-
-        fetch("https://hcaptcha.com/siteverify", {
-            method: "POST",
-            body: post_data
-        }).then(function(res_2) {
-            return res_2.json();
-        }).then(function(text) {
-            let captcha_pass = 0;
-            if(hcaptcha !== '') {
-                if(text.success === true) {
-                    captcha_pass = 1;
-                }
-            } else {
-                captcha_pass = 1;
-            }
-
-            if(captcha_pass === 1) {
-                db.all("select set_data from user_data where user_name = ? and set_name = 'password' limit 1", [user_name], function(err, db_data) {
-                    if(db_data.length === 0) {
-                        res.json({
-                            "req" : "error",
-                            "reason" : "user_name not exist"
-                        });
-                        db.close();
-                    } else {
-                        let db_password = db_data[0].set_data;
-                        db.all("select set_data from set_data where set_name = 'secret_key'", [], function(err, db_data) {
-                            let random_key = '';
-                            if(db_data.length !== 0) {
-                                random_key = db_data[0].set_data;
-                            }
-
-                            if(db_password !== sha3_512(password + random_key)) {
-                                res.json({
-                                    "req" : "error",
-                                    "reason" : "password !== db_password"
-                                });
-                                db.close();
-                            } else {
-                                req.session['user_name'] = user_name;
-                                res.json({ "req" : "ok" });
-                                db.close();
-                            }
-                        });
-                    }
-                });
+    if(api_code) {
+        db.all("select set_name from set_data where set_name = 'api_code' and set_data = ? limit 1", [api_code], function(err, db_data) {
+            if(db_data.length !== 0) {
+                signin_val(db, req, res);
             } else {
                 res.json({
                     "req" : "error",
-                    "reason" : "captcha error"
+                    "reason" : "api code error"
                 });
                 db.close();
             }
-        }).catch(function() {
-            res.json({
-                "req" : "error",
-                "reason" : "timeout error"
-            });
-            db.close();
         });
-    });
+    } else {
+        // user_name, set_name, user_data, set_data
+        db.all("select set_data from set_data where set_name = 'hcaptcha_secret'", [], function(err, db_data_2) {
+            let hcaptcha = '';
+            if(db_data_2.length !== 0) {
+                hcaptcha = db_data_2[0].set_data;
+            }
+
+            if(hcaptcha !== '') {
+                let post_data = new URLSearchParams();
+                post_data.append('response', hcaptcha_res);
+                post_data.append('secret', hcaptcha);
+
+                fetch("https://hcaptcha.com/siteverify", {
+                    method: "POST",
+                    body: post_data
+                }).then(function(res_2) {
+                    return res_2.json();
+                }).then(function(text) {
+                    let captcha_pass = 0;
+                    if(text.success === true) {
+                        captcha_pass = 1;
+                    }
+
+                    if(captcha_pass === 1) {
+                        signin_val(db, req, res);
+                    } else {
+                        res.json({
+                            "req" : "error",
+                            "reason" : "captcha error"
+                        });
+                        db.close();
+                    }
+                }).catch(function() {
+                    res.json({
+                        "req" : "error",
+                        "reason" : "timeout error"
+                    });
+                    db.close();
+                });
+            } else {
+                signin_val(db, req, res);
+            }
+        });
+    }
 }
 
 module.exports = {
--- route/signup.js
+++ route/signup.js
@@ -2,9 +2,7 @@ const sqlite3 = require('sqlite3');
 const sha3_512 = require('js-sha3').sha3_512;
 const func = require('./func.js');
 
-function signup(req, res) {
-    const db = new sqlite3.Database(__dirname + '/../data.db');
-
+function signup_val(db, req, res) {
     let data = req.body;
 
     let user_name = data.user_name;
@@ -12,6 +10,95 @@ function signup(req, res) {
     let password = data.password;
     let password_check = data.password_check;
     let code = data.code;
+
+    if(password !== password_check) {
+        res.json({
+            "req" : "error",
+            "reason" : "password !== password_check"
+        });
+        db.close();
+    } else if(
+        user_name === '' ||
+        user_real_name === '' ||
+        password === ''
+    ) {
+        res.json({
+            "req" : "error",
+            "reason" : "empty exist"
+        });
+        db.close();
+    } else {
+        db.all("select user_name from user_data limit 1", [], function(err, db_data) {
+            let auth = '';
+            if(db_data.length === 0) {
+                auth = 'admin';
+            }
+
+            db.all("select user_name from user_data where user_name = ? limit 1", [user_name], function(err, db_data) {
+                if(db_data.length !== 0) {
+                    res.json({
+                        "req" : "error",
+                        "reason" : "user_name exist"
+                    });
+                    db.close();
+                } else {
+                    // code_id, set_name, code_data, set_data
+                    db.all("select set_name from set_data where set_name = 'code_key' and set_data = ? limit 1", [code], function(err, db_data) {
+                        let code_check = 0;
+                        if(db_data.length !== 0) {
+                            db.run("delete from set_data where set_name = 'code_key' and set_data = ?", [code]);
+
+                            code_check = 1;
+                        } else if(auth === 'admin') {
+                            code_check = 1;
+                        }
+                        
+                        if(code_check === 1) {
+                            db.all("select set_data from set_data where set_name = 'secret_key'", [], function(err, db_data) {
+                                let random_key = '';
+                                if(db_data.length !== 0) {
+                                    random_key = db_data[0].set_data;
+                                }
+                            
+                                db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'password', '', ?)", [
+                                    user_name, 
+                                    sha3_512(password + random_key)
+                                ]);
+                                db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'auth', '', ?)", [
+                                    user_name, 
+                                    auth
+                                ]);
+                                db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'user_real_name', '', ?)", [
+                                    user_name, 
+                                    user_real_name
+                                ]);
+                                db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'code', '', ?)", [
+                                    user_name, 
+                                    code
+                                ]);
+
+                                res.json({ "req" : "ok" });
+                                db.close();
+                            });
+                        } else {
+                            res.json({
+                                "req" : "error",
+                                "reason" : "code key not exist"
+                            });
+                            db.close();
+                        }
+                    });
+                }
+            });
+        });
+    }
+}
+
+function signup(req, res) {
+    const db = new sqlite3.Database(__dirname + '/../data.db');
+
+    let data = req.body;
+    
     let hcaptcha_res = data.captcha;
 
     // user_name, set_name, user_data, set_data
@@ -21,121 +108,41 @@ function signup(req, res) {
             hcaptcha = db_data_2[0].set_data;
         }
 
-        let post_data = new URLSearchParams();
-        post_data.append('response', hcaptcha_res);
-        post_data.append('secret', hcaptcha);
+        if(hcaptcha !== '') {
+            let post_data = new URLSearchParams();
+            post_data.append('response', hcaptcha_res);
+            post_data.append('secret', hcaptcha);
 
-        fetch("https://hcaptcha.com/siteverify", {
-            method: "POST",
-            body: post_data
-        }).then(function(res_2) {
-            return res_2.json();
-        }).then(function(text) {
-            let captcha_pass = 0;
-            if(hcaptcha !== '') {
+            fetch("https://hcaptcha.com/siteverify", {
+                method: "POST",
+                body: post_data
+            }).then(function(res_2) {
+                return res_2.json();
+            }).then(function(text) {
+                let captcha_pass = 0;
                 if(text.success === true) {
                     captcha_pass = 1;
                 }
-            } else {
-                captcha_pass = 1;
-            }
 
-            if(captcha_pass === 1) {
-                if(password !== password_check) {
-                    res.json({
-                        "req" : "error",
-                        "reason" : "password !== password_check"
-                    });
-                    db.close();
-                } else if(
-                    user_name === '' ||
-                    user_real_name === '' ||
-                    password === ''
-                ) {
-                    res.json({
-                        "req" : "error",
-                        "reason" : "empty exist"
-                    });
-                    db.close();
+                if(captcha_pass === 1) {
+                    signup_val(db, req, res);
                 } else {
-                    db.all("select user_name from user_data limit 1", [], function(err, db_data) {
-                        let auth = '';
-                        if(db_data.length === 0) {
-                            auth = 'admin';
-                        }
-
-                        db.all("select user_name from user_data where user_name = ? limit 1", [user_name], function(err, db_data) {
-                            if(db_data.length !== 0) {
-                                res.json({
-                                    "req" : "error",
-                                    "reason" : "user_name exist"
-                                });
-                                db.close();
-                            } else {
-                                // code_id, set_name, code_data, set_data
-                                db.all("select set_name from set_data where set_name = 'code_key' and set_data = ? limit 1", [code], function(err, db_data) {
-                                    let code_check = 0;
-                                    if(db_data.length !== 0) {
-                                        db.run("delete from set_data where set_name = 'code_key' and set_data = ?", [code]);
-
-                                        code_check = 1;
-                                    } else if(auth === 'admin') {
-                                        code_check = 1;
-                                    }
-                                    
-                                    if(code_check === 1) {
-                                        db.all("select set_data from set_data where set_name = 'secret_key'", [], function(err, db_data) {
-                                            let random_key = '';
-                                            if(db_data.length !== 0) {
-                                                random_key = db_data[0].set_data;
-                                            }
-                                        
-                                            db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'password', '', ?)", [
-                                                user_name, 
-                                                sha3_512(password + random_key)
-                                            ]);
-                                            db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'auth', '', ?)", [
-                                                user_name, 
-                                                auth
-                                            ]);
-                                            db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'user_real_name', '', ?)", [
-                                                user_name, 
-                                                user_real_name
-                                            ]);
-                                            db.run("insert into user_data (user_name, set_name, user_data, set_data) values (?, 'code', '', ?)", [
-                                                user_name, 
-                                                code
-                                            ]);
-
-                                            res.json({ "req" : "ok" });
-                                            db.close();
-                                        });
-                                    } else {
-                                        res.json({
-                                            "req" : "error",
-                                            "reason" : "code key not exist"
-                                        });
-                                        db.close();
-                                    }
-                                });
-                            }
-                        });
+                    res.json({
+                        "req" : "error",
+                        "reason" : "captcha error"
                     });
+                    db.close();
                 }
-            } else {
+            }).catch(function() {
                 res.json({
                     "req" : "error",
-                    "reason" : "captcha error"
+                    "reason" : "timeout error"
                 });
                 db.close();
-            }
-        }).catch(function() {
-            res.json({
-                "req" : "error",
-                "reason" : "timeout error"
             });
-            db.close();
-        });
+        } else {
+            signup_val(db, req, res);
+        }
     });
 }
 
