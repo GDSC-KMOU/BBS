effeed1e48b03e632105bb25a0043b7afde8709c
FelixAhn <128164325+FelixAhn@users.noreply.github.com>
2023-08-18 19:03:11 +0900

    게시판 거슬리는 디자인 수정

--- view/js/board.js
+++ view/js/board.js
@@ -1,90 +1,117 @@
 "use strict";
 
-if(document.location.pathname.startsWith('/board/')) {
-    let board_name = document.location.pathname.split('/')[2];
-    let page = document.location.pathname.split('/')[3];
-    if(page === undefined) {
-        page = '1';
+if (document.location.pathname.startsWith("/board/")) {
+    let board_name = document.location.pathname.split("/")[2];
+    let page = document.location.pathname.split("/")[3];
+    if (page === undefined) {
+        page = "1";
     }
-    let pageButton = '';
-    fetch("/api/board_length/" + url_encode(board_name)).then((res) => {
-        return res.json();
-    }).then((data) => {
-        let pageNum = Math.ceil(data.length / 20);
-        for(let i =1; i <= pageNum; i++){
-            pageButton += `<a class="text-decoration-none text-success p-1 m-1" href="/board/${url_encode(board_name)}/${String(i)}">${i}</a>`;
-        }  
-    });
-    
-    let boardDisplay = '';
-        const boardState = ((e) => {
-            switch(e) {
-                case 'main':
-                    boardDisplay = '공지사항'
-                    break
-                case 'talk':
-                    boardDisplay = '기술'
-                    break
-                case 'free':
-                    boardDisplay = '자유'
-                    break
-                case 'secret':
-                    boardDisplay = '비공개'
-                    break
+    let pageButton = "";
+    fetch("/api/board_length/" + url_encode(board_name))
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            let pageNum = Math.ceil(data.length / 20);
+            for (let i = 1; i <= pageNum; i++) {
+                pageButton += `<a class="text-decoration-none text-success p-1 m-1" href="/board/${url_encode(
+                    board_name
+                )}/${String(i)}">${i}</a>`;
             }
-            return boardDisplay;
-    });
+        });
 
-    fetch("/api/board/" + url_encode(board_name) + "/" + url_encode(page)).then(function (res) {
-        return res.json();
-    }).then(function (text) {
-        let data = '';
+    let boardDisplay = "";
+    const boardState = (e) => {
+        switch (e) {
+            case "main":
+                boardDisplay = "공지사항";
+                break;
+            case "talk":
+                boardDisplay = "기술";
+                break;
+            case "free":
+                boardDisplay = "자유";
+                break;
+            case "secret":
+                boardDisplay = "비공개";
+                break;
+        }
+        return boardDisplay;
+    };
 
-        let now = new Date().getTime();
-        let the_str = '0';
-        for(let for_a = 0; for_a < text.length; for_a++) {
-            let past_time = new Date(text[for_a]['date'].replace(/-/g, '/'));
-            past_time = past_time.getTime();
-            
-            let the_time = new Date(now - past_time); 
-            the_time = the_time / 1000; //ms->s
-            
-            if(the_time < 60){
-                the_str = String(Math.floor(the_time)) + "초 전";
-            } else if(the_time < 3600){ //1h
-                the_str = String(Math.floor(the_time / 60)) + "분 전";
-            } else if(the_time < 86400){ //1d
-                the_str = String(Math.floor(the_time / 3600)) + "시간 전";
-            } else if(the_time < 604800){  //7d
-                the_str = String(Math.floor(the_time / 86400)) + "일 전";
-            } else if(the_time < 2592000){ //30d
-                the_str = String(Math.floor(the_time / 604800)) + "주일 전";
-            } else if(the_time < 7776000){ //90d
-                the_str = String(Math.floor(the_time / 2592000)) + "달 전";
-            } else{
-                the_str = Number(the_str);
-                the_str = text[for_a]['date'].split(' ')[0];
-            }
+    fetch("/api/board/" + url_encode(board_name) + "/" + url_encode(page))
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (text) {
+            let data = "";
 
-            data += `
+            let now = new Date().getTime();
+            let the_str = "0";
+            for (let for_a = 0; for_a < text.length; for_a++) {
+                let past_time = new Date(
+                    text[for_a]["date"].replace(/-/g, "/")
+                );
+                past_time = past_time.getTime();
+
+                let the_time = new Date(now - past_time);
+                the_time = the_time / 1000; //ms->s
+
+                if (the_time < 60) {
+                    the_str = String(Math.floor(the_time)) + "초 전";
+                } else if (the_time < 3600) {
+                    //1h
+                    the_str = String(Math.floor(the_time / 60)) + "분 전";
+                } else if (the_time < 86400) {
+                    //1d
+                    the_str = String(Math.floor(the_time / 3600)) + "시간 전";
+                } else if (the_time < 604800) {
+                    //7d
+                    the_str = String(Math.floor(the_time / 86400)) + "일 전";
+                } else if (the_time < 2592000) {
+                    //30d
+                    the_str = String(Math.floor(the_time / 604800)) + "주일 전";
+                } else if (the_time < 7776000) {
+                    //90d
+                    the_str = String(Math.floor(the_time / 2592000)) + "달 전";
+                } else {
+                    the_str = Number(the_str);
+                    the_str = text[for_a]["date"].split(" ")[0];
+                }
+
+                data +=
+                    `
                 <tr>
-                    <td ><a class="text-decoration-none text-success ms-1" href="/board_read/` + url_encode(board_name) + '/' + text[for_a]['doc_id'] + `">` + xss_filter(text[for_a]['title']) + `</a></td>
-                    <td class="text-center">` + the_str + `</td>
-                    <td class="text-center">` + xss_filter(text[for_a]['user_name_real']) + `</td>
+                    <td ><a class="text-decoration-none text-success ms-1" href="/board_read/` +
+                    url_encode(board_name) +
+                    "/" +
+                    text[for_a]["doc_id"] +
+                    `">` +
+                    xss_filter(text[for_a]["title"]) +
+                    `</a></td>
+                    <td class="text-center">` +
+                    the_str +
+                    `</td>
+                    <td class="text-center">` +
+                    xss_filter(text[for_a]["user_name_real"]) +
+                    `</td>
                 </tr>
             `;
-        }
-        
-        document.getElementById('main_data').innerHTML = `
+            }
+
+            document.getElementById("main_data").innerHTML =
+                `
         <div class="container-xxl p-3">
             <div class="rounded-5 p-3 mb-2 d-flex justify-content-start align-items-center pagetop__div"style="background-color: #dbebe1">
                 <h4 class="mb-0 ms-3 fw-bold">${boardState(board_name)}</h4>
             </div>
         </div>
             <section id="board">
-                <div class="container-xxl p-3">                
+                <div class="container-xxl mb-3 p-3">                
                     <div class="d-flex row justify-content-between board-content gap-3">
-                        ` + bbs_nav() + `
+                        ` +
+                bbs_nav() +
+                `
                         <div class="col-xxl-9 p-3 shadow rounded-5 board__right">
                             <div class="container px-1">
                                 <div class="table-responsive">
@@ -103,18 +130,24 @@ if(document.location.pathname.startsWith('/board/')) {
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            ` + data + `
+                                            ` +
+                data +
+                `
                                         </tbody>
                                     </table>
                                     <div class="d-flex justify-content-between">
                                     <div></div>
                                     <div>
-                                        `+ pageButton +`
+                                        ` +
+                pageButton +
+                `
                                     </div>
                                     <div>
                                     <p class="text-end fw-bold mb-0">
                                         <span class="board__write px-1">
-                                            <a href="/board_add/` + url_encode(board_name) + `" class="text-decoration-none">
+                                            <a href="/board_add/` +
+                url_encode(board_name) +
+                `" class="text-decoration-none">
                                             <i class="fa-solid fa-pencil"></i>
                                             글쓰기
                                             </a>
@@ -130,5 +163,5 @@ if(document.location.pathname.startsWith('/board/')) {
                 </div>
             </section>
         `;
-    });
+        });
 }
--- view/js/board_read.js
+++ view/js/board_read.js
@@ -1,135 +1,239 @@
 "use strict";
 
 function bbs_delete_content() {
-    let board_name = document.location.pathname.split('/')[2];
-    let board_id = document.location.pathname.split('/')[3];
+    let board_name = document.location.pathname.split("/")[2];
+    let board_id = document.location.pathname.split("/")[3];
 
-    document.getElementById('board_delete_content').addEventListener("click", function() {
-        let remove_content = confirm("글을 삭제하겠습니까?");
-        if(remove_content === true) {
-            fetch("/api/board_edit/" + url_encode(board_name) + '/' + url_encode(board_id), {
-                method : 'POST',
-                headers : { 'Content-Type': 'application/json' },
-                body : JSON.stringify({
-                    'title' : '',
-                    'content' : ''
-                })
-            }).then(function(res) {
-                return res.json();
-            }).then(function(text) {
-                if(text.req === 'ok') {
-                    document.location.pathname = '/board/' + url_encode(board_name);
-                } else {
-                    alert(text.req + '\n' + text.reason);
-                }
-            });
-        }
-    });
+    document
+        .getElementById("board_delete_content")
+        .addEventListener("click", function () {
+            let remove_content = confirm("글을 삭제하겠습니까?");
+            if (remove_content === true) {
+                fetch(
+                    "/api/board_edit/" +
+                        url_encode(board_name) +
+                        "/" +
+                        url_encode(board_id),
+                    {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            title: "",
+                            content: "",
+                        }),
+                    }
+                )
+                    .then(function (res) {
+                        return res.json();
+                    })
+                    .then(function (text) {
+                        if (text.req === "ok") {
+                            document.location.pathname =
+                                "/board/" + url_encode(board_name);
+                        } else {
+                            alert(text.req + "\n" + text.reason);
+                        }
+                    });
+            }
+        });
 }
 
 function bbs_delete_comment(doc_id) {
-    let board_name = document.location.pathname.split('/')[2];
-    let board_id = document.location.pathname.split('/')[3];
+    let board_name = document.location.pathname.split("/")[2];
+    let board_id = document.location.pathname.split("/")[3];
 
-    document.getElementById('board_comment_delete_' + doc_id).addEventListener("click", function() {
-        let remove_content = confirm("글을 삭제하겠습니까?");
-        if(remove_content === true) {
-            console.log("/api/board_comment_edit/" + url_encode(board_name) + '/' + url_encode(board_id) + '/' + url_encode(doc_id));
-            fetch("/api/board_comment_edit/" + url_encode(board_name) + '/' + url_encode(board_id) + '/' + url_encode(doc_id), {
-                method : 'POST',
-                headers : { 'Content-Type': 'application/json' },
-                body : JSON.stringify({
-                    'content' : ''
-                })
-            }).then(function(res) {
-                return res.json();
-            }).then(function(text) {
-                if(text.req === 'ok') {
-                    document.location.pathname = '/board_read/' + url_encode(board_name) + '/' + url_encode(board_id);
-                } else {
-                    alert(text.req + '\n' + text.reason);
-                }
-            });
-        }
-    });
+    document
+        .getElementById("board_comment_delete_" + doc_id)
+        .addEventListener("click", function () {
+            let remove_content = confirm("글을 삭제하겠습니까?");
+            if (remove_content === true) {
+                console.log(
+                    "/api/board_comment_edit/" +
+                        url_encode(board_name) +
+                        "/" +
+                        url_encode(board_id) +
+                        "/" +
+                        url_encode(doc_id)
+                );
+                fetch(
+                    "/api/board_comment_edit/" +
+                        url_encode(board_name) +
+                        "/" +
+                        url_encode(board_id) +
+                        "/" +
+                        url_encode(doc_id),
+                    {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json" },
+                        body: JSON.stringify({
+                            content: "",
+                        }),
+                    }
+                )
+                    .then(function (res) {
+                        return res.json();
+                    })
+                    .then(function (text) {
+                        if (text.req === "ok") {
+                            document.location.pathname =
+                                "/board_read/" +
+                                url_encode(board_name) +
+                                "/" +
+                                url_encode(board_id);
+                        } else {
+                            alert(text.req + "\n" + text.reason);
+                        }
+                    });
+            }
+        });
 }
 
 function bbs_comment() {
-    let board_name = document.location.pathname.split('/')[2];
-    let board_id = document.location.pathname.split('/')[3];
+    let board_name = document.location.pathname.split("/")[2];
+    let board_id = document.location.pathname.split("/")[3];
 
-    document.getElementById('board_comment').addEventListener("click", function() {
-        let content = document.getElementById('board_add_content').value;
+    document
+        .getElementById("board_comment")
+        .addEventListener("click", function () {
+            let content = document.getElementById("board_add_content").value;
 
-        fetch("/api/board_comment_edit/" + url_encode(board_name) + '/' + url_encode(board_id), {
-            method : 'POST',
-            headers : { 'Content-Type': 'application/json' },
-            body : JSON.stringify({
-                'content' : content
-            })
-        }).then(function(res) {
-            return res.json();
-        }).then(function(text) {
-            if(text.req === 'ok') {
-                document.location.pathname = '/board_read/' + url_encode(board_name) + '/' + url_encode(board_id);
-            } else {
-                alert(text.req + '\n' + text.reason);
-            }
+            fetch(
+                "/api/board_comment_edit/" +
+                    url_encode(board_name) +
+                    "/" +
+                    url_encode(board_id),
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        content: content,
+                    }),
+                }
+            )
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (text) {
+                    if (text.req === "ok") {
+                        document.location.pathname =
+                            "/board_read/" +
+                            url_encode(board_name) +
+                            "/" +
+                            url_encode(board_id);
+                    } else {
+                        alert(text.req + "\n" + text.reason);
+                    }
+                });
         });
-    });
 }
 
-if(document.location.pathname.startsWith('/board_read/')) {
-    let board_name = document.location.pathname.split('/')[2];
-    let board_id = document.location.pathname.split('/')[3];
+if (document.location.pathname.startsWith("/board_read/")) {
+    let board_name = document.location.pathname.split("/")[2];
+    let board_id = document.location.pathname.split("/")[3];
+    let boardDisplay = "";
+    const boardState = (e) => {
+        switch (e) {
+            case "main":
+                boardDisplay = "공지사항";
+                break;
+            case "talk":
+                boardDisplay = "기술";
+                break;
+            case "free":
+                boardDisplay = "자유";
+                break;
+            case "secret":
+                boardDisplay = "비공개";
+                break;
+        }
+        return boardDisplay;
+    };
 
-    fetch("/api/board_read/" + url_encode(board_name) + '/' + url_encode(board_id)).then(function(res) {
-        return res.json();
-    }).then(function(text_1) {
-        fetch("/api/board_comment/" + url_encode(board_name) + '/' + url_encode(board_id)).then(function(res) {
+    fetch(
+        "/api/board_read/" + url_encode(board_name) + "/" + url_encode(board_id)
+    )
+        .then(function (res) {
             return res.json();
-        }).then(function(text_2) {
-            let comment = '';
-            for(let for_a = 0; for_a < text_2.length; for_a++) {
-                comment += `
+        })
+        .then(function (text_1) {
+            fetch(
+                "/api/board_comment/" +
+                    url_encode(board_name) +
+                    "/" +
+                    url_encode(board_id)
+            )
+                .then(function (res) {
+                    return res.json();
+                })
+                .then(function (text_2) {
+                    let comment = "";
+                    for (let for_a = 0; for_a < text_2.length; for_a++) {
+                        comment +=
+                            `
                     <div class="border p-3 rounded-4 bg-light">
                         <p class="px-2 mb-2 fw-bold comment__title">
-                            ` + xss_filter(text_2[for_a].user_name_real) + `
+                            ` +
+                            xss_filter(text_2[for_a].user_name_real) +
+                            `
                             <span style="float: right;">
-                                ` + text_2[for_a].date + `
+                                ` +
+                            text_2[for_a].date +
+                            `
                                 ·
-                                <a id="board_comment_delete_` + text_2[for_a].doc_id + `" class="text-decoration-none text-success" href="javascript:void(0);">
+                                <a id="board_comment_delete_` +
+                            text_2[for_a].doc_id +
+                            `" class="text-decoration-none text-success" href="javascript:void(0);">
                                     <i class="fa-solid fa-trash text-success"></i>
                                     삭제
                                 </a>
                             </span>
                         </p>
-                        <p>` + text_2[for_a].render_content + `</p>
+                        <p>` +
+                            text_2[for_a].render_content +
+                            `</p>
                     </div>
                     <br>
                 `;
-            }
+                    }
 
-            document.getElementById('main_data').innerHTML = `
+                    document.getElementById("main_data").innerHTML =
+                        `
+                        <div class="container-xxl p-3">
+            <div class="rounded-5 p-3 mb-2 d-flex justify-content-start align-items-center pagetop__div"style="background-color: #dbebe1">
+                <h4 class="mb-0 ms-3 fw-bold">${boardState(board_name)}</h4>
+            </div>
+        </div>
                 <section id="board">
-                    <div class="container-xxl p-3 board-content">
-                        <div class="row gap-5">
-                            ` + bbs_nav() + `
-                            <div class="col-xxl-9 p-4 shadow rounded-5">
+                    <div class="container-xxl mb-3 p-3 board-content">
+                        <div class="d-flex row justify-content-between board-content gap-3">
+                            ` +
+                        bbs_nav() +
+                        `
+                            <div class="col-xxl-9 p-3 shadow rounded-5 board__right">
                                 <div class="container px-1">
-                                    <h3 class="mb-0">` + xss_filter(text_1.title) + `</h3>
+                                    <h3 class="mb-0">` +
+                        xss_filter(text_1.title) +
+                        `</h3>
                                         <div class="d-flex justify-content-between border-bottom py-2">
                                             <div>
-                                                <span class="board__add-write">`+ xss_filter(text_1.user_name_real) + `</span>
-                                                <span class="board__add-date px-3">` + text_1.date + `</sapn>
+                                                <span class="board__add-write">` +
+                        xss_filter(text_1.user_name_real) +
+                        `</span>
+                                                <span class="board__add-date px-3">` +
+                        text_1.date +
+                        `</sapn>
                                             </div>
                                             <div>
                                                 <span class="boardread_left">
-                                                    <a class="text-decoration-none text-success" href="/board_edit/` + url_encode(board_name) + `/` + url_encode(board_id) + `">
+                                                    <a class="text-decoration-none text-success" href="/board_edit/` +
+                        url_encode(board_name) +
+                        `/` +
+                        url_encode(board_id) +
+                        `">
                                                         <i class="fa-solid fa-pen-to-square text-success"></i>    
                                                         수정
                                                     </a>
-                                                    ·
+                                                    /
                                                     <a id="board_delete_content" class="text-decoration-none text-success" href="javascript:void(0);">
                                                         <i class="fa-solid fa-trash text-success"></i>
                                                         삭제
@@ -137,15 +241,20 @@ if(document.location.pathname.startsWith('/board_read/')) {
                                                 </span>
                                             </div>
                                         </div>
-                                    <p class="lead">` + text_1.render_content + `</p>
+                                    <p class="lead">` +
+                        text_1.render_content +
+                        `</p>
                                     <hr>
-                                    ` + comment + `
-                                    <div class="border p-3 rounded-4 bg-light" style="height: 200px;">
+                                    ` +
+                        comment +
+                        `
+                                    <div class="border p-3 rounded-4 bg-light" style="height: 185px;">
                                         <p class="px-2 mb-2 fw-bold comment__title">댓글</p>
                                         <textarea id="board_add_content" class="form-control form-control-sm mb-2" rows="4" placeholder="댓글 내용을 입력하세요." aria-label="내용" ></textarea>
                                         
-                                        <button type="submit" class="btn btn-success" id="board_comment">저장</button> 
-                                        <button type="submit" class="btn btn-outline-success me-2" id="board_add_preview">미리보기</button>
+                                        <button type="submit" class="btn btn-outline-success px-2 py-0" id="board_add_preview" style="float:right;">미리보기</button>
+                                        <button type="submit" class="btn btn-success px-2 py-0 me-2"id="board_comment" style="float:right;">저장</button> 
+                                        
                                     </div>
                                     <br>
                                     <div id="board_add_preview_field"></div>
@@ -155,17 +264,17 @@ if(document.location.pathname.startsWith('/board_read/')) {
                     </div>
                 </section>
             `;
-            
-            hljs.highlightAll();
-            
-            func_board_preview();
-            
-            bbs_delete_content();
-            bbs_comment();
 
-            for(let for_a = 0; for_a < text_2.length; for_a++) {
-                bbs_delete_comment(text_2[for_a].doc_id);
-            }
+                    hljs.highlightAll();
+
+                    func_board_preview();
+
+                    bbs_delete_content();
+                    bbs_comment();
+
+                    for (let for_a = 0; for_a < text_2.length; for_a++) {
+                        bbs_delete_comment(text_2[for_a].doc_id);
+                    }
+                });
         });
-    });
 }
