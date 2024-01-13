d0ecffc56ef730ac691fd4d8fe768b4d05cbb42e
잉여개발기 (SPDV) <min08101@naver.com>
2024-01-13 21:21:37 +0900

    갱신

--- route/chat.js
+++ route/chat.js
@@ -3,7 +3,17 @@ const func = require('./func.js');
 
 function chat_load(db, req, res, chat_id, for_a = 0, end_data = []) {
     if(chat_id.length > for_a) {
-        chat_load(db, req, res, chat_id, for_a + 1, end_data);
+        end_data[for_a] = {};
+        db.all("select set_name, set_data, user_data from chat_data where user_id = ?", [chat_id[for_a]], function(err, db_data) {
+            console.log(chat_id[for_a], db_data);
+            for(let for_b = 0; for_b < db_data.length; for_b++) {
+                end_data[for_a][db_data[for_b].set_name] = db_data[for_b].set_data;
+                end_data[for_a]['user_id'] = db_data[for_b].user_data;
+            }
+            end_data[for_a]['id'] = chat_id[for_a];
+
+            chat_load(db, req, res, chat_id, for_a + 1, end_data);
+        });
     } else {
         res.json({
             "req" : "ok",
@@ -18,45 +28,18 @@ function chat(req, res) {
 
     let get_id = req.params.get_id;
     if(!get_id) {
-        get_id = '';
+        get_id = '0';
     }
 
     func.user_check(db, req, res, function(user_name) {
-        if(get_id === 'test') {
-            res.json({
-                "req" : "ok",
-                "data" : [
-                    {
-                        "id" : "1",
-                        "chat" : "test",
-                        "user_id" : "min08101",
-                        "date" : "2024-01-13 11:00:00"
-                    }, {
-                        "id" : "2",
-                        "chat" : "test2",
-                        "user_id" : "min08101",
-                        "date" : "2024-01-13 11:00:02"
-                    }, {
-                        "id" : "3",
-                        "chat" : "test3",
-                        "user_id" : "min08101",
-                        "date" : "2024-01-13 11:00:04"
-                    }
-                ]
-            });
-            db.close();
-        } else if(get_id === '') {
-            db.all("select user_id from chat_data where set_name = 'chat' order by user_id + 0 asc", [], function(err, db_data) {
-                let chat_id = [];
-                for(let for_a = 0; for_a < db_data.length; for_a++) {
-                    chat_id.push(db_data[for_a].user_id);
-                }
+        db.all("select user_id from chat_data where set_name = 'chat' and user_id + 0 >= ? + 0 order by user_id + 0 asc", [get_id], function(err, db_data) {
+            let chat_id = [];
+            for(let for_a = 0; for_a < db_data.length; for_a++) {
+                chat_id.push(db_data[for_a].user_id);
+            }
 
-                chat_load(db, req, res, chat_id);
-            });
-        } else {
-            
-        }
+            chat_load(db, req, res, chat_id);
+        });
     });
 }
 
